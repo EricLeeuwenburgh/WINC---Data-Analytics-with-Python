c80b647389adbf92d1bf00a3461031ad2c6b1626
Eric Leeuwenburgh <eric_leeuwenburgh@hotmail.com>
2023-03-08 13:37:42 +0100

    Added comments and strings

---
+++ Exercises/comments/main.py
@@ -0,0 +1,16 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+# Bankaccount 
+# Calculation of yearly interest
+account_balance = 10524     # amount in Euro's
+interest_rate = 0.0050      # 0.5 percent
+total = account_balance + account_balance * interest_rate
+
+print(total)
+
+""" This is a simple calculation an bankaccount balance + interest """
+""" Copyright - Eric Leeuwenburgh """
